fd9ba0c0dc2373c43f4e4031bb1b328a8a51be92
donggun_Kal <0429donggun@gmail.com>
2020-01-26 21:04:32 +0900

    비밀정보추가

---
+++ config/prod.js
@@ -0,0 +1,4 @@
+// heroku 에서 가져오게됨
+module.exports = {
+    mongoURI: process.env.MONGO_URI
+}
--- index.js
+++ index.js
@@ -1,16 +1,40 @@
-const express = require('express')
-const app = express()
-const port = 5000
+const express = require('express');
+const app = express();
+const port = 5000;
+const bodyParser = require("body-parser");
 
-const mongoose = require('mongoose')
-mongoose.connect('mongodb+srv://Admin:1111@react-blog-zkxvu.mongodb.net/test?retryWrites=true&w=majority', {
-    useNewUrlParser: true,
-    useUnifiedTopology: true,
-    useCreateIndex: true,
-    useFindAndModify: false
+const config = require('./config/key');
+
+const { User } = require("./models/User");
+
+// application/x-www-form-urlencoded 타입 분석 (form 데이터타입)
+app.use(bodyParser.urlencoded({ extended: true }));
+
+// application/json 타입 분석
+app.use(bodyParser.json());
+
+const mongoose = require('mongoose');
+mongoose.connect(config.mongoURI, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+  useCreateIndex: true,
+  useFindAndModify: false
 }).then(() => console.log('MongoDB Connected'))
   .catch(err => console.log(err))
 
 app.get('/', (req, res) => res.send('Hello World!'))
 
-app.listen(port, () => console.log(`Example app listening on port ${port}!`))
+app.post('/register', (req, res) => {
+  //회원 가입 할때 필요한 정보들을 client에서 가져오면 그것들을 DB에 넣어준다.
+
+  const user = new User(req.body);
+  user.save((err, userInfo) => {
+    if (err) return res.json({ success: false, err });
+    return res.status(200).json({
+      success: true
+    })
+  });
+})
+
+
+app.listen(port, () => console.log(`Example app listening on port ${port}!`));
